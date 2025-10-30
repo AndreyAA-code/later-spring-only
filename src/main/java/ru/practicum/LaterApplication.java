42d1dbd446449409306f26f15e057bd91a11cd5c
AndreyAA-code <otd53@yandex.ru>
2025-10-30 22:45:38 +0300

    start

--- src/main/java/ru/practicum/LaterApplication.java
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -1,6 +1,8 @@
 package ru.practicum;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
 
 public class LaterApplication {
@@ -10,7 +12,15 @@ public class LaterApplication {
 
         Tomcat tomcat = new Tomcat();
         tomcat.getConnector().setPort(PORT);
+        // то самое «приложение» или «контекст» с пустым путём
+        Context tomcatContext = tomcat.addContext("", null);
 
+        // класс Wrapper позволяет задать дополнительные настройки для сервлета
+        Wrapper testServletWrapper =
+                Tomcat.addServlet(tomcatContext, "testServlet", new TestServlet());
+
+        // addMapping() сопоставляет URL-путь с сервлетом
+        testServletWrapper.addMapping("/test");
         tomcat.start();
     }
 }
