add81a7e3c4f506594720480bb1113bdca6f936e
AndreyAA-code <otd53@yandex.ru>
2025-11-17 12:46:58 +0300

    added transaction to management and serviceImpls

--- src/main/java/ru/practicum/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/user/UserServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
