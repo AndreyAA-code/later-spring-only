460b59a54004960bebbc9933cd135403a5f8c408
AndreyAA-code <otd53@yandex.ru>
2025-11-20 12:52:47 +0300

    added transaction to management and serviceImpls checked userDto and itemDto mappers added JPQL queries in items db added ItemNote class with al others: contr -> repo

--- src/main/java/ru/practicum/note/ItemNoteRepository.java
+++ src/main/java/ru/practicum/note/ItemNoteRepository.java
@@ -1,10 +1,13 @@
 package ru.practicum.note;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemNoteRepository extends JpaRepository<ItemNote, Long>, QuerydslPredicateExecutor<ItemNote> {
 
@@ -12,4 +15,7 @@ public interface ItemNoteRepository extends JpaRepository<ItemNote, Long>, Query
 
     @Query ("SELECT itNote FROM ItemNote as itNote JOIN itNote.item as i WHERE i.user.id =?1 and ?2 member of i.tags ")
     List<ItemNote> findByTagAndUser(Long userId, String tag);
+
+    Page<ItemNote> findAllByItemUserId(Long itemUserId, Pageable pageRequest);
+
 }
--- src/main/java/ru/practicum/note/ItemNoteServiceImpl.java
+++ src/main/java/ru/practicum/note/ItemNoteServiceImpl.java
@@ -1,8 +1,11 @@
 package ru.practicum.note;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.item.Item;
 import ru.practicum.item.ItemRepository;
 
 import java.util.List;
@@ -12,10 +15,14 @@ import java.util.List;
 @Transactional(readOnly=true)
 public class ItemNoteServiceImpl implements ItemNoteService {
     private final ItemNoteRepository itemNoteRepository;
+    private final ItemRepository itemRepository;
 
     @Override
-    public ItemNote addNewItemNote(Long userId, ItemNoteDto itemNoteDto) {
-        return null;
+    public ItemNoteDto addNewItemNote(Long userId, ItemNoteDto itemNoteDto) {
+        Item item = itemRepository.findById(itemNoteDto.getItemId())
+                .orElseThrow(() -> new RuntimeException("Item not found"));
+        ItemNote itemNote = itemNoteRepository.save(ItemNoteMapper.mapToItemNote(itemNoteDto, item));
+        return ItemNoteMapper.mapToItemNoteDto(itemNote);
     }
 
     @Override
@@ -32,7 +39,10 @@ public class ItemNoteServiceImpl implements ItemNoteService {
 
     @Override
     public List<ItemNoteDto> listAllItemsWithNotes(Long userId, int from, int size) {
-        return List.of();
+        PageRequest pageRequest = PageRequest.of(from, size);
+        return itemNoteRepository.findAllByItemUserId(userId, pageRequest)
+                .map(ItemNoteMapper::mapToItemNoteDto)
+                .getContent();
     }
 
 }
