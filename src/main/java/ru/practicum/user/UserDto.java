1e4582ccaec54f1b7d2bc63f760e22e9f7ab141b
AndreyAA-code <otd53@yandex.ru>
2025-12-03 19:54:07 +0300

    тест заработал

--- src/main/java/ru/practicum/user/UserDto.java
+++ src/main/java/ru/practicum/user/UserDto.java
@@ -1,10 +1,12 @@
 package ru.practicum.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.Instant;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class UserDto {
@@ -12,6 +14,7 @@ public class UserDto {
     private String email;
     private String firstName;
     private String lastName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     private Instant registrationDate;
     private UserState state;
 
